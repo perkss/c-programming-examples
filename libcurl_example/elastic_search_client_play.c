78eba71f7a06f5d8f7225792924127da361eca5f
perkss <stuart.m.perks@gmail.com>
2020-02-24 17:09:30 +0000

    Some C curl with callbacks and CMAKE

--- libcurl_example/elastic_search_client_play.c
+++ libcurl_example/elastic_search_client_play.c
@@ -1,177 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <curl/curl.h>
-
-struct WriteThis {
-    const char *readptr;
-    size_t sizeleft;
-};
-
-static size_t read_callback(void *dest, size_t size, size_t nmemb, void *userp) {
-    struct WriteThis *wt = (struct WriteThis *) userp;
-    size_t buffer_size = size * nmemb;
-
-    if (wt->sizeleft) {
-        /* copy as much as possible from the source to the destination */
-        printf("Copying data");
-        size_t copy_this_much = wt->sizeleft;
-        if (copy_this_much > buffer_size)
-            copy_this_much = buffer_size;
-        memcpy(dest, wt->readptr, copy_this_much);
-
-        wt->readptr += copy_this_much;
-        wt->sizeleft -= copy_this_much;
-        return copy_this_much; /* we copied this many bytes */
-    }
-
-    return 0; /* no more data left to deliver */
-}
-
-static const char data[] = "{ \"id\": 1, \"name\": \"dark\" }";
-
-struct MemoryStruct {
-    char *memory;
-    size_t size;
-};
-
-static size_t
-WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;
-    struct MemoryStruct *mem = (struct MemoryStruct *) userp;
-
-    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
-    if (ptr == NULL) {
-        /* out of memory! */
-        printf("not enough memory (realloc returned NULL)\n");
-        return 0;
-    }
-
-    mem->memory = ptr;
-    memcpy(&(mem->memory[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->memory[mem->size] = 0;
-
-    return realsize;
-}
-
-// https://curl.haxx.se/libcurl/c/post-callback.html for getting the data written entered
-void put_request_with_callback(CURL *curl, CURLcode res) {
-
-    // this holds the response
-    struct MemoryStruct chunk;
-
-    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
-    chunk.size = 0;    /* no data at this point */
-
-    // TODO this holds the payload for writing data
-    struct WriteThis wt;
-
-    // Assing the data to be posted
-    wt.readptr = data;
-    wt.sizeleft = strlen(data);
-
-    printf("\nPUT WT is filled with request data %s\n", wt.readptr);
-
-    curl = curl_easy_init();
-
-    /* First set the URL that is about to receive our POST. */
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9200/chocolate/_doc/1");
-
-    /* Now specify we want to POST data */
-    curl_easy_setopt(curl, CURLOPT_PUT, 1L);
-
-    /* we want to use our own read function */
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
-
-
-    /* pointer to pass to our read function */
-    curl_easy_setopt(curl, CURLOPT_READDATA, &wt);
-
-    /* get verbose debug output please */
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "Accept: application/json");
-    headers = curl_slist_append(headers, "Content-Type: application/json");
-    headers = curl_slist_append(headers, "charsets: utf-8");
-
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
-
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-    /*
-      If you use POST to a HTTP 1.1 server, you can send data without knowing
-      the size before starting the POST if you use chunked encoding. You
-      enable this by adding a header like "Transfer-Encoding: chunked" with
-      CURLOPT_HTTPHEADER. With HTTP 1.0 or without chunked transfer, you must
-      specify the size in the request.
-    */
-#ifdef USE_CHUNKED
-    {
-      struct curl_slist *chunk = NULL;
-
-      chunk = curl_slist_append(chunk, "Transfer-Encoding: chunked");
-      res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-      /* use curl_slist_free_all() after the *perform() call to free this
-         list again */
-    }
-#else
-    /* Set the expected POST size. If you want to POST large amounts of data,
-       consider CURLOPT_POSTFIELDSIZE_LARGE */
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) wt.sizeleft);
-#endif
-
-#ifdef DISABLE_EXPECT
-    /*
-      Using POST with HTTP 1.1 implies the use of a "Expect: 100-continue"
-      header.  You can disable this header with CURLOPT_HTTPHEADER as usual.
-      NOTE: if you want chunked transfer too, you need to combine these two
-      since you can only set one list of headers with CURLOPT_HTTPHEADER. */
-
-    /* A less good option would be to enforce HTTP 1.0, but that might also
-       have other implications. */
-    {
-      struct curl_slist *chunk = NULL;
-
-      chunk = curl_slist_append(chunk, "Expect:");
-
-        printf("Headers back put");
-      res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-      /* use curl_slist_free_all() after the *perform() call to free this
-         list again */
-    }
-#endif
-
-    /* Perform the request, res will get the return code */
-
-    res = curl_easy_perform(curl);
-    /* Check for errors */
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-    } else {
-        /*
-         * Now, our chunk.memory points to a memory block that is chunk.size
-         * bytes big and contains the remote file.
-         *
-         * Do something nice with it!
-         */
-
-        printf("%lu bytes retrieved\n", (unsigned long) chunk.size);
-        printf("Post data retrieved is %s\n", chunk.memory);
-
-    }
-
-    printf("\nPUT WT is empty as sent data and emptied buffer %s\n", wt.readptr);
-    /* always cleanup */
-    curl_easy_cleanup(curl);
-}
-
 
 /**
  * Calls elastic search with a PUT request to install our JSON.
@@ -206,49 +34,21 @@ void put_request(CURL *curl, CURLcode res) {
     }
 }
 
-// https://curl.haxx.se/libcurl/c/getinmemory.html
-// example of the write call back
 /**
  * Gets the Indexed Document
  */
 void get_request(CURL *curl_handle, CURLcode res) {
 
-    struct MemoryStruct chunk;
-
-    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
-    chunk.size = 0;    /* no data at this point */
-
     curl_handle = curl_easy_init();
 
     if (curl_handle) {
-
         curl_easy_setopt(curl_handle, CURLOPT_URL, "http://localhost:9200/chocolate/_doc/1");
-
-        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
-        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *) &chunk);
-
-        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
         res = curl_easy_perform(curl_handle);
 
         if (res != CURLE_OK) {
             fprintf(stderr, "CURL EASY PERFORM RETURNED %s\n", curl_easy_strerror(res));
-        } else {
-            /*
-             * Now, our chunk.memory points to a memory block that is chunk.size
-             * bytes big and contains the remote file.
-             *
-             * Do something nice with it!
-             */
-
-            printf("%lu bytes retrieved\n", (unsigned long) chunk.size);
-            printf("data retrieved is %s\n", chunk.memory);
-
         }
-
-        free(chunk.memory);
-
 
         curl_easy_cleanup(curl_handle);
     }
@@ -259,16 +59,10 @@ int main(void) {
     CURL *curl_handle;
     CURLcode res;
 
-    res = curl_global_init(CURL_GLOBAL_DEFAULT);
-    /* Check for errors */
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_global_init() failed: %s\n",
-                curl_easy_strerror(res));
-        return 1;
-    }
+    curl_global_init(CURL_GLOBAL_ALL);
 
     /* Index the document */
-    put_request_with_callback(curl_handle, res);
+    put_request(curl_handle, res);
 
     /* Get the indexed document */
     get_request(curl_handle, res);
